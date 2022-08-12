b94aba4cd2bf0f3b0cc3c9274be822701c7f641d
Subash Ganti <srinivasganti@srinivass-MacBook-Pro.local>
2022-08-12 17:04:24 +0530

    Retrieving info from client successful

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express');
-
 const app = express();
-
+const bodyParser = require('body-parser');
 var port  = 3000;
 
-app.use(express.static("public"));
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+// post requests
 app.post("/",function(req,res){
     res.sendFile(__dirname+"/booking.html");
+    console.log(req.body);
 });
 
 app.post("/signup",function(req,res){
     res.send("Successfully signed up.")
+    console.log(req.body);
 });
 
 app.post("/login",function(req,res){
     res.send("Login Page");
+    console.log(req.body);
 });
 
 app.listen(port,function(){
